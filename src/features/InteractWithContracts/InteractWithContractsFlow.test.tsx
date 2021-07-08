4c15d4be76c63d329e2224589c8d4afde7cebf55
Frederik Bolding <frederik.bolding@gmail.com>
2021-07-08 15:16:08 +0200

    Add more component tests and fix a few small bugs

--- src/features/InteractWithContracts/InteractWithContractsFlow.test.tsx
+++ src/features/InteractWithContracts/InteractWithContractsFlow.test.tsx
@@ -3,20 +3,28 @@ import React from 'react';
 import selectEvent from 'react-select-event';
 import { APP_STATE, fireEvent, mockAppState, simpleRender, waitFor } from 'test-utils';
 
-import { fAccounts, fAssets, fContracts } from '@fixtures';
+import { fAccount, fAccounts, fAssets, fContracts } from '@fixtures';
 import { translateRaw } from '@translations';
 
 import InteractWithContractsFlow from './InteractWithContractsFlow';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({
-    pathname: '/interact-with-contracts'
-  })
-}));
+jest.mock('@vendor', () => {
+  return {
+    ...jest.requireActual('@vendor'),
+    FallbackProvider: jest.fn().mockImplementation(() => ({
+      estimateGas: jest.fn().mockResolvedValue(21000),
+      getTransactionCount: jest.fn().mockResolvedValue(10),
+      getFeeData: jest.fn().mockResolvedValue({
+        maxFeePerGas: '20000000000',
+        maxPriorityFeePerGas: '1000000000'
+      })
+    }))
+  };
+});
 
 function getComponent() {
   return simpleRender(<InteractWithContractsFlow />, {
+    initialRoute: '/interact-with-contracts',
     initialState: mockAppState({
       accounts: fAccounts,
       assets: fAssets,
@@ -33,8 +41,8 @@ describe('InteractWithContractsFlow', () => {
     ).toBeInTheDocument();
   });
 
-  it('can generate form for a contract', async () => {
-    const { getByText } = getComponent();
+  it('can submit form', async () => {
+    const { getByText, container } = getComponent();
     await selectEvent.openMenu(
       getByText(translateRaw('CONTRACT_SELECTION_PLACEHOLDER'), { exact: false })
     );
@@ -58,5 +66,23 @@ describe('InteractWithContractsFlow', () => {
 
     await waitFor(() => expect(getByText('_version')).toBeInTheDocument());
     await waitFor(() => expect(getByText('_resolver')).toBeInTheDocument());
+
+    fireEvent.change(container.querySelector('input[name="_version"]')!, {
+      target: {
+        value: '1'
+      }
+    });
+
+    fireEvent.change(container.querySelector('input[name="_resolver"]')!, {
+      target: {
+        value: fAccount.address
+      }
+    });
+
+    fireEvent.click(getByText(translateRaw('ACTION_17')));
+
+    await waitFor(() =>
+      expect(getByText(translateRaw('CONFIRM_TX_MODAL_TITLE'))).toBeInTheDocument()
+    );
   });
 });
